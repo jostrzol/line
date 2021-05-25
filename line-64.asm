a643dba7256c72624da71d8bc0648207122323f2
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2021-05-25 17:10:53 +0200

    change operations to be 32 bit (something is not yes with second endpoint)

--- line-64.asm
+++ line-64.asm
@@ -28,57 +28,57 @@
 ;3 - rfrcprt for x0, frcprt for x1
 %macro	endpnt	3
 	;calculate xend
-	mov	rbx, %1		;rbx = x (for later)
+	mov	ebx, %1		;ebx = x (for later)
 	round	%1		;x <- round(x) = xend = xpx
 	;calculate yend
-	mov	rax, %1		;rax = xend
-	sub	rax, rbx	;rax = xend - x
-	imul	r11		;edx:eax = slope * (xend - x)
-	shrd	eax, edx, 16	;shift result back to place
-	lea	%2, [%2+rax]	;y <- y + slope * (xend - x) = yend
+	mov	eax, %1		;eax = xend
+	sub	eax, ebx	;eax = xend - x
+	imul	eax, r11d	;edx:eax = slope * (xend - x)
+	shr	eax, 16		;shift result back to place
+	lea	%2, [%2+eax]	;y <- y + slope * (xend - x) = yend
 	;calculate xgap
-	lea	rcx, [rbx+0x8000]	;rcx = x + 0.5
-	%3	rcx			;rcx = (r)frcprt(x + 0.5) = xgap
+	lea	ecx, [ebx+0x8000]	;ecx = x + 0.5
+	%3	ecx			;ecx = (r)frcprt(x + 0.5) = xgap
 	;calculate ypx, xpx
-	mov	rdx, %2		;rdx = yend
-	intprt	rdx		;rdx = intprt(yend) = ypx
-	mov	rbx, %1		;rbx = xpx
+	mov	edx, %2		;edx = yend
+	intprt	edx		;edx = intprt(yend) = ypx
+	mov	ebx, %1		;ebx = xpx
 	;calculate first bufpos
-	test	r13, r13	;if steep
-	mov	rax, rbx	;rax = xpx
-	cmovnz	rbx, rdx	;xpx <-> ypx
-	cmovnz	rdx, rax	;xpx <-> ypx
-				;now rdx = ycoord, rbx = xcoord
-	shr	rbx, 16		;shift xcoord to place
-	shr	rdx, 16		;shift ycoord to place
+	test	r13d, r13d	;if steep
+	mov	eax, ebx	;eax = xpx
+	cmovnz	ebx, edx	;xpx <-> ypx
+	cmovnz	edx, eax	;xpx <-> ypx
+				;now edx = ycoord, ebx = xcoord
+	shr	ebx, 16		;shift xcoord to place
+	shr	edx, 16		;shift ycoord to place
 
-	imul	rdx, r12	;rdx = ycoord * [stride]		:move ycoord pixels "up"
-	lea	rdx, [rdx+rbx]	;rdx = ycoord * [stride] + xpx	:move xcoord pixels "right"
+	imul	edx, r12d	;edx = ycoord * [stride]		:move ycoord pixels "up"
+	lea	edx, [edx+ebx]	;edx = ycoord * [stride] + xpx	:move xcoord pixels "right"
 
 	lea	rdx, [rdx+rdi]	;rdx += canvas			:make bufpos absolute
 	;calculate the base color for this endpoint (will be used like color in main loop)
-	imul	rcx, r9		;rcx = xgap * [color]
-	round	rcx		;rcx = round(xgap * [color])
-	shr	rcx, 16		;rcx = round(xgap * [color]) >> 16 = basecolor
+	imul	ecx, r9d	;ecx = xgap * [color]
+	round	ecx		;ecx = round(xgap * [color])
+	shr	ecx, 16		;ecx = round(xgap * [color]) >> 16 = basecolor
 	;calculate first color
-	mov	rbx, %2		;rbx = yend
-	rfrcprt	rbx		;rbx = rfrcprt(yend)
-	imul	rbx, rcx	;rbx = rfrcprt(yend) * basecolor
-	round	rbx		;rbx = round(rfrcprt(yend) * basecolor)
-	shr	rbx, 16		;rbx = round(rfrcprt(yend) * basecolor) >> 16 = firstcolor
+	mov	ebx, %2		;ebx = yend
+	rfrcprt	ebx		;ebx = rfrcprt(yend)
+	imul	ebx, ecx	;ebx = rfrcprt(yend) * basecolor
+	round	ebx		;ebx = round(rfrcprt(yend) * basecolor)
+	shr	ebx, 16		;ebx = round(rfrcprt(yend) * basecolor) >> 16 = firstcolor
 	;paint first pixel
 	mov	[rdx], bl
 	;calculate second bufpos
 	mov	rax, rdx	;rax = rdx = old_bufpos
 	lea	rdx, [rdx+r12]	;rdx = old_bufpos + [stride], which is old_bufpos moved "up" by 1px
 	inc	rax		;rax = old_bufpos + 1, which is old_bufpos moved "right" by 1px
-	test	r13, r13	;if steep => rdx <- rax
+	test	r13d, r13d	;if steep => rdx <- rax
 	cmovnz	rdx, rax	;rdx = bufpos
 	;check if out of bounds
 	cmp	rdx, r10
 	jae	%%end
 	;calculate second color
-	sub	rcx, rbx	;rcx = basecolor - firstcolor = secondcolor
+	sub	ecx, ebx	;ecx = basecolor - firstcolor = secondcolor
 	;paint second pixel
 	mov	[rdx], cl
 %%end:
@@ -128,37 +128,37 @@ line:
 	mov	r15, rcx
 
 ;	calculate steep
-	mov     rax, r8
-	sub     rax, r14
-	mov	rbx, rax
-	neg	rbx
-	cmovns	rax, rbx	;rax = abs(y1-y0)
+	mov     eax, r8d
+	sub     eax, r14d
+	mov	ebx, eax
+	neg	ebx
+	cmovns	eax, ebx	;eax = abs(y1-y0)
 
-	mov	rbx, r15
-	sub     rbx, rsi
-	mov	rcx, rbx
-	neg	rcx
-	cmovns	rbx, rcx	;rbx = abs(x1-x0)
+	mov	ebx, r15d
+	sub     ebx, esi
+	mov	ecx, ebx
+	neg	ecx
+	cmovns	ebx, ecx	;ebx = abs(x1-x0)
 
-	xor	r13, r13
-	cmp	rax, rbx
-	mov	rcx, 1
-	cmovg	r13, rcx	;r13 = steep
+	xor	r13d, r13d
+	cmp	eax, ebx
+	mov	ecx, 1
+	cmovg	r13d, ecx	;r13 = steep
 
 ;	swap endpoints if needed
 
 	;if steep:	x <-> y
 	jng	no_steep
-	xchg	rsi, r14	;x0 <-> y0
-	xchg	r15, r8		;x1 <-> y1
+	xchg	esi, r14d	;x0 <-> y0
+	xchg	r15d, r8d	;x1 <-> y1
 	;endif
 no_steep:
 
-	cmp	rsi, r15
+	cmp	esi, r15d
 	;if x0>x1:	_0 <-> _1
 	jng	x0gx1
-	xchg	rsi, r15	;x0 <-> x1
-	xchg	r14, r8		;y0 <-> y1
+	xchg	esi, r15d	;x0 <-> x1
+	xchg	r14d, r8d	;y0 <-> y1
 	;endif
 x0gx1:
 
@@ -180,44 +180,44 @@ x0gx1:
 	;divide
 	idiv	rcx		;rax = dy/dx = slope
 	;save slope
-	mov	r11, rax
+	mov	r11d, eax
 
 ;	handle first endpoint
-	endpnt	rsi, r14, rfrcprt
+	endpnt	esi, r14d, rfrcprt
 
 ;	handle second endpoint
-	endpnt	r15, r8, frcprt
+	endpnt	r15d, r8d, frcprt
 
 ;	main loop (for x from xpx0+1 to xpx1-1)
-	shr	rsi, 16		;shift x to be regular int
-	shr	r15, 16		;shift xpx1 to be regular int
+	shr	esi, 16		;shift x to be regular int
+	shr	r15d, 16	;shift xpx1 to be regular int
 
-	test	r13, r13	;if steep
+	test	r13d, r13d	;if steep
 	jnz	steep_loop
 loop:
 	;move to next point
-	inc	rsi		;x++
-	lea	r14, [r14+r11]	;move y to next intersection
+	inc	esi			;x++
+	lea	r14d, [r14d+r11d]	;move y to next intersection
 
 	;do until x < x1
-	cmp	rsi, r15	
+	cmp	esi, r15d
 	jae	end
 
 	;calculate first bufpos
-	mov	rcx, r14	;rcx = y
-	intprt	rcx		;rcx = intprt(y) = ypx
-	shr	rcx, 16		;shift ypx to place
+	mov	ecx, r14d	;ecx = y
+	intprt	ecx		;ecx = intprt(y) = ypx
+	shr	ecx, 16		;shift ypx to place
 
-	imul	rcx, r12	;rcx = ypx * [stride]		:move ypx pixels "up"
-	lea	rcx, [rcx+rsi]	;rcx = ypx * [stride] + xpx	:move xpx pixels "right"
+	imul	ecx, r12d	;ecx = ypx * [stride]		:move ypx pixels "up"
+	lea	ecx, [ecx+esi]	;ecx = ypx * [stride] + xpx	:move xpx pixels "right"
 
 	lea	rcx, [rcx+rdi]	;rcx = bufpos			:make bufpos absolute
 	;calculate first color
-	mov	rdx, r14	;rdx = y
-	rfrcprt	rdx		;rdx = rfrcprt(y)
-	imul	rdx, r9		;rdx = rfrcprt(y) * [color]
-	round	rdx		;rdx = round(rfrcprt(y) * [color])
-	shr	rdx, 16		;rdx = round(rfrcprt(y) * [color]) >> 16 = firstcolor
+	mov	edx, r14d	;edx = y
+	rfrcprt	edx		;edx = rfrcprt(y)
+	imul	edx, r9d	;edx = rfrcprt(y) * [color]
+	round	edx		;edx = round(rfrcprt(y) * [color])
+	shr	edx, 16		;edx = round(rfrcprt(y) * [color]) >> 16 = firstcolor
 	;paint first pixel
 	mov	[rcx], dl
 	;calculate second bufpos
@@ -226,8 +226,8 @@ loop:
 	cmp	rcx, r10
 	jae	loop
 	;calculate second color
-	neg	rdx		;rdx = -firstcolor
-	lea	rdx, [rdx+r9]	;rdx = [color] - firstcolor = secondcolor
+	neg	edx		;edx = -firstcolor
+	lea	edx, [edx+r9d]	;edx = [color] - firstcolor = secondcolor
 	;paint second pixel
 	mov	[rcx], dl
 
@@ -235,30 +235,30 @@ loop:
 
 steep_loop:
 	;move to next point
-	inc	rsi		;x++
-	lea	r14, [r14+r11]	;move y to next intersection
+	inc	esi			;x++
+	lea	r14d, [r14d+r11d]	;move y to next intersection
 
 	;do until x < x1
-	cmp	rsi, r15
+	cmp	esi, r15d
 	jae	end
 
 	;calculate first bufpos
-	mov	rdx, r14	;rdx = y
-	intprt	rdx		;rdx = intprt(y) = ypx
-	shr	rdx, 16		;shift ypx to place
+	mov	edx, r14d	;edx = y
+	intprt	edx		;edx = intprt(y) = ypx
+	shr	edx, 16		;shift ypx to place
 
-	mov	rcx, rsi	;rcx = x = xpx
+	mov	ecx, esi	;ecx = x = xpx
 
-	imul	rcx, r12	;rcx = xpx * [stride]		:move xpx pixels "up"
-	lea	rcx, [rcx+rdx]	;rcx = xpx * [stride] + ypx	:move ypx pixels "right"
+	imul	ecx, r12d	;ecx = xpx * [stride]		:move xpx pixels "up"
+	lea	ecx, [ecx+edx]	;ecx = xpx * [stride] + ypx	:move ypx pixels "right"
 
 	lea	rcx, [rcx+rdi]	;rcx = bufpos			:make bufpos absolute
 	;calculate first color
-	mov	rdx, r14	;rdx = y
-	rfrcprt	rdx		;rdx = rfrcprt(y)
-	imul	rdx, r9		;rdx = rfrcprt(y) * [color]
-	round	rdx		;rdx = round(rfrcprt(y) * [color])
-	shr	rdx, 16		;rdx = round(rfrcprt(y) * [color]) >> 16 = firstcolor
+	mov	edx, r14d	;edx = y
+	rfrcprt	edx		;edx = rfrcprt(y)
+	imul	edx, r9d	;edx = rfrcprt(y) * [color]
+	round	edx		;edx = round(rfrcprt(y) * [color])
+	shr	edx, 16		;edx = round(rfrcprt(y) * [color]) >> 16 = firstcolor
 	;paint first pixel
 	mov	[rcx], dl
 	;calculate second bufpos
@@ -267,8 +267,8 @@ steep_loop:
 	cmp	rcx, r10
 	jae	steep_loop
 	;calculate second color
-	neg	rdx		;rdx = -firstcolor
-	lea	rdx, [rdx+r9]	;rdx = [color] - firstcolor = secondcolor
+	neg	edx		;edx = -firstcolor
+	lea	edx, [edx+r9d]	;edx = [color] - firstcolor = secondcolor
 	;paint second pixel
 	mov	[rcx], dl
 
